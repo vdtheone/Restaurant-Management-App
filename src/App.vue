e85dfc28fcfdffdf0cf64cdd24870ade131a359a
Vishal Prajapati <vp22052001@gmail.com>
2023-02-22 14:13:03 +0530

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <Header />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/HeaderPage.vue';
+
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0px;
+  padding: 0px;
+}
+
+.register,.login,.add{
+    text-align: center;
+}
+
+.logo{
+    width: 125px;
+    margin-top:0px;
+}
+
+.register input, .login input,.add input{
+    width: 270px;
+    display: block;
+    margin: 20px auto; 
+    height: 35px;
+    border: 1px solid skyblue;
+    padding-left: 20px;
+    font-size: 18px;
+}
+
+.register button,.login button, .add button{
+    width: 270px;
+    height: 30px;
+    font-size: 18px;
+    /* background-color: rgb(46, 101, 123); */
+}
+
+.login a,.register a{
+  margin-top: 50px;
+  font-size: 20px;
+}
+
+p{
+    margin: 15px;
 }
 </style>
